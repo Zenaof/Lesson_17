cc713fc4c9902b40f8b7c9b216cf7c78fea203f0
USER PC4 <muravev592@gmail.com>
2024-01-26 18:18:41 +0300

    first commit

---
+++ list_generators.py
@@ -0,0 +1,12 @@
+import random
+
+
+#age = ['leo', 'max', 'kate', 'mag']
+numbers = [2, 5, 7, 34, 12]
+#a = [age for i in age if i > 10 and i < 21]
+# a = []
+# for i in numbers:
+#         a.append(i.upper())
+
+
+print(a)
